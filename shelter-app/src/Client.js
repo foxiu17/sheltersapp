00c7efe7878b87acaf2c0880dc41e7adb282ad83
Marcin Foks <mfoks@softfusion.pl>
2020-02-01 19:37:46 +0100

    fix client uri

--- shelter-app/src/Client.js
+++ shelter-app/src/Client.js
@@ -1,7 +1,15 @@
-import ApolloClient from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+console.log("application mode: ", process.env.NODE_ENV);
+console.log("application PORT: ", process.env.PORT);
+
+let appMode = process.env.NODE_ENV || "development";
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/api',
+  uri:
+    appMode !== "development"
+      ? "https://shelters-app.herokuapp.com/api"
+      : "http://localhost:8000/api",
   cache: new InMemoryCache()
 });
